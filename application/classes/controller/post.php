4378b133de94855a9c27b66495461de2085aed94
Allan G. Atim <aatim@blueant@example.org>
2013-01-11 22:31:07 +0800

    ilagay na yung ws call papunta kay gspi

--- application/classes/controller/post.php
+++ application/classes/controller/post.php
@@ -8,38 +8,61 @@ class Controller_Post extends Controller {
 		if (!empty ($post_vars))
 		{
 			$session['id'] = 99;
-			$files = FALSE;
+			$ws_image      = array('key' => '0a82e8ef64084435841ee3073ddaeda8745787f9');
+			$unlink_file   = FALSE;
 
 			if (isset($_FILES['source']['name']) && !empty($_FILES['source']['name']))
 			{
-				$files = Validation::factory($_FILES);
-				$directory = 'asset/i/';
-
-				// get extension name
-				$file_tokens = explode('.',$files['source']['name']);
-				$filename = date('mdyhms').'_00'.$session['id'].'_'.time().'.'.$file_tokens[1];
-
-				Upload::save($files['source'],$filename,$directory);
-				$url = $directory.$filename;
+				$ws_image['photo']     = '@'.$_FILES['source']['tmp_name'];
+				$ws_image['name']      = $_FILES['source']['name'];
 			}
 			else
 			{
-				$url = $this->request->post('url');
+				$url_file  = pathinfo($post_vars['url']);
+				$save_path = DOCROOT.'asset/c/';
+				$iraw      = file_get_contents($post_vars['url']);
+
+				// i-save yung file locally para maipasa sa WS, tapus alisin din mamaya
+				$fp = fopen($save_path.$url_file['basename'],'w');
+				fputs($fp,$iraw);
+				fclose($fp);
+
+				$ws_image['photo']     = '@'.$save_path.$url_file['basename'];
+				$ws_image['name']      = $url_file['basename'];
+
+				$unlink_file = TRUE;
 			}
 
+			// i-upload ang image using gspi-ws
+			$ch = curl_init();
+			$options = array(
+				CURLOPT_URL => 'http://gspi/api/upload_photo',
+				CURLOPT_POST => 1,
+				CURLOPT_POSTFIELDS => $ws_image,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_VERBOSE => 1
+			);
+			curl_setopt_array($ch, $options);
+			$response = curl_exec($ch);
+			curl_close($ch);
+
+			$image = simplexml_load_string($response);
 
 			$article = array(
 				'user_id'     => 99,
 				'category'    => $this->request->post('category'),
 				'title'       => $this->request->post('title'),
-				'picture'     => $url,
+				'picture'     => $image->uid,
 				'description' => $this->request->post('description'),
 			);
-			Helper_Util::pr($article);
 			$a = new Model_Article;
 			$a->values($article);
 			$a->save();
-			Helper_Util::pr($this->request->post());
+
+			if ($unlink_file)
+			{
+				unlink($save_path.$url_file['basename']);
+			}
 		}
 		else
 		{
@@ -51,35 +74,71 @@ class Controller_Post extends Controller {
 	{
 		if ($this->request->post())
 		{
-			$session = Session::instance()->get('user',NULL);
+			$session     = Session::instance()->get('user',NULL);
+			$ws_image    = array('key' => '0a82e8ef64084435841ee3073ddaeda8745787f9');
+			$unlink_file = FALSE;
 
 			// ayusin yung file uploads nung mga picture
 			if (isset($_FILES['source']['name']) && !empty($_FILES['source']['name']))
 			{
-				$files = Validation::factory($_FILES);
-				$directory = 'asset/i/';
-
-				// get extension name
-				$file_tokens = explode('.',$files['source']['name']);
-				$filename = date('mdyhms').'_00'.$session['id'].'_'.time().'.'.$file_tokens[1];
-
-				Upload::save($files['source'],$filename,$directory);
-				$url = $directory.$filename;
+//				$files = Validation::factory($_FILES);
+//				$directory = 'asset/i/';
+//
+//				// get extension name
+//				$file_tokens = explode('.',$files['source']['name']);
+//				$filename = date('mdyhms').'_00'.$session['id'].'_'.time().'.'.$file_tokens[1];
+//
+//				Upload::save($files['source'],$filename,$directory);
+//				$url = $directory.$filename;
+				$ws_image['photo']     = '@'.$_FILES['source']['tmp_name'];
+				$ws_image['name']      = $_FILES['source']['name'];
 			}
 			else
 			{
-				$url = $this->request->post('url');
+				$url_file  = pathinfo($this->request->post('url'));
+				$save_path = DOCROOT.'asset/c/';
+				$iraw      = file_get_contents($this->request->post('url'));
+
+				// i-save yung file locally para maipasa sa WS, tapus alisin din mamaya
+				$fp = fopen($save_path.$url_file['basename'],'w');
+				fputs($fp,$iraw);
+				fclose($fp);
+
+				$ws_image['photo']     = '@'.$save_path.$url_file['basename'];
+				$ws_image['name']      = $url_file['basename'];
+
+				$unlink_file = TRUE;
 			}
+
+			// i-upload ang image using gspi-ws
+			$ch = curl_init();
+			$options = array(
+				CURLOPT_URL => 'http://gspi/api/upload_photo',
+				CURLOPT_POST => 1,
+				CURLOPT_POSTFIELDS => $ws_image,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_VERBOSE => 1
+			);
+			curl_setopt_array($ch, $options);
+			$response = curl_exec($ch);
+			curl_close($ch);
+
+			$image = simplexml_load_string($response);
 
 			$article = new Model_Article;
 			$article->values(array(
 				'user_id'     => $session['id'],
 				'category'    => $this->request->post('category'),
 				'title'       => $this->request->post('title'),
-				'picture'     => $url,
+				'picture'     => $image->uid,
 				'description' => $this->request->post('description'),
 			));
 			$article->save();
+
+			if ($unlink_file)
+			{
+				unlink($save_path.$url_file['basename']);
+			}
 
 			// error checking after saving data and log what happen here
 			if ($article->id)
